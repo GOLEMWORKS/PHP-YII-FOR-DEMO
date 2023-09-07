c0f3da3192daf1735c236c4a7ee3e58289e7bc5f
GOLEMWORKS <korotkikh.vladimir.isr21.1@gmail.com>
2023-09-07 14:53:56 +0200

    Say.php commit 1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionSay($message = 'Привет')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
 }
