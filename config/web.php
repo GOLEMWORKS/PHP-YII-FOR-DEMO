122e549044d77d9351087b8e07bf17996f9daec2
GOLEMWORKS <korotkikh.vladimir.isr21.1@gmail.com>
2023-09-15 12:39:07 +0200

    uncomment, красивые ссылки на страницы контроллеров

--- config/web.php
+++ config/web.php
@@ -42,14 +42,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
